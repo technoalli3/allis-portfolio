ca63dfe13dfeefde229889126ff69dc4fbded2a2
technoalli3 <chloebenner1900@gmail.com>
2026-08-19 09:20:56 -0400

    testing metadata

--- metadata.js
+++ metadata.js
@@ -1,8 +1,15 @@
 (function() {
     const pageTitlesPlaceholder = document.querySelectorAll('[metadata-title-placeholder]');
+    const pageDescriptionsPlaceholder = document.querySelectorAll('[metadata-description-placeholder]');
+    const pageDescription = document.querySelector('p[description]').textContent;
     const pageTitle = document.title;
 
     pageTitlesPlaceholder.forEach((placeholder) => {
         placeholder.setAttribute('content', pageTitle);
+        console.log(`Set metadata title to ${pageTitle}`);
+    });
+    pageDescriptionsPlaceholder.forEach((placeholder) => {
+        placeholder.setAttribute('content', pageDescription);
+        console.log(`Set metadata description to ${pageDescription}`);
     });
 })();
